3d0cb4650bef49d1823738d02513d3f497c46413
Dhriti Dhall <74966124+DhRiTiD@users.noreply.github.com>
2022-06-02 17:48:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBNUxZJ_lS_gKY-0aDY3eSmweoL7ytwvm8",
+  authDomain: "storytellingapp-42cfb.firebaseapp.com",
+  databaseURL: "https://storytellingapp-42cfb-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-42cfb",
+  storageBucket: "storytellingapp-42cfb.appspot.com",
+  messagingSenderId: "881270612660",
+  appId: "1:881270612660:web:bf7ae2e2583c46d7672de1"
+};
